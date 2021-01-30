d5a9030cc1a1292590609bbc9c89caeb5ce9e207
MMena88 <paradalaw@yahoo.com>
2021-01-30 00:33:27 -0600

    version2

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-return render_template("index.htm")
+    return render_template("index.htm")
 
 
 if __name__ == "__main__":
-app.run(debug=True)
+    app.run(debug=True)
